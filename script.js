98351072f8ad3215042708e40bc5b23e82205e21
Ioseph-Santos <158473132+Ioseph-Santos@users.noreply.github.com>
2024-02-01 18:12:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,6 @@
 //     delay: anime.stagger(200, {grid: [79, 5], from: 'center'})
 
 //   });
-import anime from 'node_modules/animejs/lib/anime.es.js';
-const anime = require('animejs');
   // ------------------------------------------------ Mudando de cor o nome -------------------------------------------
   var titulo = document.querySelector("#h1")
   var span = document.querySelector(".span")
